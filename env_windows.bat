5fb3cb054edf22689527910e9ef4c4d579cf5294
zeng guoqing <zgq_iot@163.com>
2016-11-09 16:57:04 +0800

    添加windows平台环境变量设置脚本

---
+++ env_windows.bat
@@ -0,0 +1,4 @@
+@echo on
+set GOPATH=%GOPATH%;%GOBOOK%\\chapter1\\calcproj;%GOBOOK%\\chapter2\\sorter;%GOBOOK%\\chapter3\\mplayer;%GOBOOK%\\chapter4\\cgss
+set PATH=%PATH%;%GOBOOK%\\chapter1\\calcproj\\bin;%GOBOOK%\\chapter2\\sorter\\bin;%GOBOOK%\\chapter3\\mplayer\\bin;%GOBOOK%\\chapter4\\cgss\\bin
+pause
