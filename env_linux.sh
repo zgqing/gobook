b04e74b16584b753850cadd127f3d19ee693a6f5
zgqing <zgq_iot@163.com>
2016-11-12 00:04:24 +0800

    修改macos环境变量配置

--- env_linux.sh
+++ env_linux.sh
@@ -1,3 +1,4 @@
+export GOBOOK=/Users/zgq/Documents/src/go/gobook
 export GOPATH=$GOPATH:$GOBOOK/chapter1/calcproj:$GOBOOK/chapter2/sorter:$GOBOOK/chapter3/mplayer:$GOBOOK/chapter4/cgss
 export PATH=$PATH:$GOBOOK/chapter1/calcproj/bin:$GOBOOK/chapter2/sorter/bin:$GOBOOK/chapter3/mplayer/bin:$GOBOOK/chapter4/cgss/bin
 
